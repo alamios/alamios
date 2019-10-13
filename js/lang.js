19c0f000537a58497b7ae206fa6a716f1d11afef
alamios <alamios@protonmail.com>
2019-10-13 23:11:20 +0200

    fix save

--- js/lang.js
+++ js/lang.js
@@ -31,15 +31,26 @@ function switchLang() {
         setTextID("design", "Diseñado por");
         setTextID("about-intro", "Hola!");
         setTextID("about-text", "\
-            En un lugar de la Mancha, de cuyo nombre no quiero acordarme,\
-            no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero,\
-            adarga antigua, rocín flaco y galgo corredor.");
+            En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo\
+            que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo\
+            corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y\
+            quebrantos los sábados, lentejas los viernes, algún palomino de añadidura los domingos,\
+            consumían las tres partes de su hacienda. El resto della concluían sayo de velarte,\
+            calzas de velludo para las fiestas con sus pantuflos de lo mismo, los días de entre\
+            semana se honraba con su vellori de lo más fino. Tenía en su casa una ama que pasaba\
+            de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza,\
+            que así ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo\
+            con los cincuenta años, era de complexión recia, seco de carnes, enjuto de rostro; gran\
+            madrugador y amigo de la caza. Quieren decir que tenía el sobrenombre de Quijada o Quesada\
+            (que en esto hay alguna diferencia en los autores que deste caso escriben), aunque por\
+            conjeturas verosímiles se deja entender que se llama Quijana; pero esto importa poco a\
+            nuestro cuento; basta que en la narración dél no se salga un punto de la verdad.");
         setTextID("home-intro", "Inicio");
         setTextID("projects-intro", "Proyectos");
     }
     else {
         setTextClass("nav", ["Home", "Projects", "About", "Contact"]);
-        setTextID("bio", "\
+        setTextID("bio", "Software and Web developer<br>\
             In a village of La Mancha, the name of which I have no desire to call to mind,\
             there lived not long since one of those gentlemen that keep a lance in the lance-rack,\
             an old buckler, a lean hack, and a greyhound for coursing.");
@@ -48,7 +59,19 @@ function switchLang() {
         setTextID("about-text", "\
             In a village of La Mancha, the name of which I have no desire to call to mind,\
             there lived not long since one of those gentlemen that keep a lance in the lance-rack,\
-            an old buckler, a lean hack, and a greyhound for coursing.");
+            an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef\
+            than mutton, a salad on most nights, scraps on Saturdays, lentils on Fridays,\
+            and a pigeon or so extra on Sundays, made away with three-quarters of his income.\
+            The rest of it went in a doublet of fine cloth and velvet breeches and shoes to match\
+            for holidays, while on week-days he made a brave figure in his best homespun.\
+            He had in his house a housekeeper past forty, a niece under twenty, and a lad for the\
+            field and market-place, who used to saddle the hack as well as handle the bill-hook.\
+            The age of this gentleman of ours was bordering on fifty; he was of a hardy habit,\
+            spare, gaunt-featured, a very early riser and a great sportsman. They will have it his\
+            surname was Quixada or Quesada (for here there is some difference of opinion among the\
+            authors who write on the subject), although from reasonable conjectures it seems plain\
+            that he was called Quexana. This, however, is of but little importance to our tale;\
+            it will be enough not to stray a hair’s breadth from the truth in the telling of it.");
         setTextID("home-intro", "Home");
         setTextID("projects-intro", "Projects");
     }
