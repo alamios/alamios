1cde02c14b60de5fd638ce0f90d79799dfeaeb1f
alamios <alamios@protonmail.com>
2020-09-21 00:21:27 +0200

    updated texts

--- js/lang.js
+++ js/lang.js
@@ -23,11 +23,9 @@ function switchLangHome() {
 function switchLangProjects() {
     if (window.rqlang == "es") {
         setContentName("title", "alamios&nbsp;&nbsp;|&nbsp;&nbsp;Proyectos");
-        setContentSelection("#projects .intro", "Proyectos");
     }
     else {
         setContentName("title", "alamios&nbsp;&nbsp;|&nbsp;&nbsp;Projects");
-        setContentSelection("#projects .intro", "Projects");
     }
 }
 
@@ -35,77 +33,13 @@ function switchLangAbout() {
     if (window.rqlang == "es") {
         setContentName("title", "alamios&nbsp;&nbsp;|&nbsp;&nbsp;Sobre mi");
         setContentSelection("#about .intro", "Hola!");
-        setContentSelection("#about .text", "\
-            En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo\
-            que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo\
-            corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y\
-            quebrantos los sábados, lentejas los viernes, algún palomino de añadidura los domingos,\
-            consumían las tres partes de su hacienda. El resto della concluían sayo de velarte,\
-            calzas de velludo para las fiestas con sus pantuflos de lo mismo, los días de entre\
-            semana se honraba con su vellori de lo más fino. Tenía en su casa una ama que pasaba\
-            de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza,\
-            que así ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo\
-            con los cincuenta años, era de complexión recia, seco de carnes, enjuto de rostro; gran\
-            madrugador y amigo de la caza. Quieren decir que tenía el sobrenombre de Quijada o Quesada\
-            (que en esto hay alguna diferencia en los autores que deste caso escriben), aunque por\
-            conjeturas verosímiles se deja entender que se llama Quijana; pero esto importa poco a\
-            nuestro cuento; basta que en la narración dél no se salga un punto de la verdad.\
-            En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo\
-            que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo\
-            corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y\
-            quebrantos los sábados, lentejas los viernes, algún palomino de añadidura los domingos,\
-            consumían las tres partes de su hacienda. El resto della concluían sayo de velarte,\
-            calzas de velludo para las fiestas con sus pantuflos de lo mismo, los días de entre\
-            semana se honraba con su vellori de lo más fino. Tenía en su casa una ama que pasaba\
-            de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza,\
-            que así ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo\
-            con los cincuenta años, era de complexión recia, seco de carnes, enjuto de rostro; gran\
-            madrugador y amigo de la caza. Quieren decir que tenía el sobrenombre de Quijada o Quesada\
-            (que en esto hay alguna diferencia en los autores que deste caso escriben), aunque por\
-            conjeturas verosímiles se deja entender que se llama Quijana; pero esto importa poco a\
-            nuestro cuento; basta que en la narración dél no se salga un punto de la verdad.\
-            En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo\
-            que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo\
-            corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y\
-            quebrantos los sábados, lentejas los viernes, algún palomino de añadidura los domingos,\
-            consumían las tres partes de su hacienda. El resto della concluían sayo de velarte,\
-            calzas de velludo para las fiestas con sus pantuflos de lo mismo, los días de entre\
-            semana se honraba con su vellori de lo más fino. Tenía en su casa una ama que pasaba\
-            de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza,\
-            que así ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo\
-            con los cincuenta años, era de complexión recia, seco de carnes, enjuto de rostro; gran\
-            madrugador y amigo de la caza. Quieren decir que tenía el sobrenombre de Quijada o Quesada\
-            (que en esto hay alguna diferencia en los autores que deste caso escriben), aunque por\
-            conjeturas verosímiles se deja entender que se llama Quijana; pero esto importa poco a\
-            nuestro cuento; basta que en la narración dél no se salga un punto de la verdad.\
-            En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo\
-            que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo\
-            corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y\
-            quebrantos los sábados, lentejas los viernes, algún palomino de añadidura los domingos,\
-            consumían las tres partes de su hacienda. El resto della concluían sayo de velarte,\
-            calzas de velludo para las fiestas con sus pantuflos de lo mismo, los días de entre\
-            semana se honraba con su vellori de lo más fino. Tenía en su casa una ama que pasaba\
-            de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza,\
-            que así ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo\
-            con los cincuenta años, era de complexión recia, seco de carnes, enjuto de rostro; gran\
-            madrugador y amigo de la caza. Quieren decir que tenía el sobrenombre de Quijada o Quesada\
-            (que en esto hay alguna diferencia en los autores que deste caso escriben), aunque por\
-            conjeturas verosímiles se deja entender que se llama Quijana; pero esto importa poco a\
-            nuestro cuento; basta que en la narración dél no se salga un punto de la verdad.\
-            En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo\
-            que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo\
-            corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y\
-            quebrantos los sábados, lentejas los viernes, algún palomino de añadidura los domingos,\
-            consumían las tres partes de su hacienda. El resto della concluían sayo de velarte,\
-            calzas de velludo para las fiestas con sus pantuflos de lo mismo, los días de entre\
-            semana se honraba con su vellori de lo más fino. Tenía en su casa una ama que pasaba\
-            de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza,\
-            que así ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo\
-            con los cincuenta años, era de complexión recia, seco de carnes, enjuto de rostro; gran\
-            madrugador y amigo de la caza. Quieren decir que tenía el sobrenombre de Quijada o Quesada\
-            (que en esto hay alguna diferencia en los autores que deste caso escriben), aunque por\
-            conjeturas verosímiles se deja entender que se llama Quijana; pero esto importa poco a\
-            nuestro cuento; basta que en la narración dél no se salga un punto de la verdad.");
+        setContentSelection("#about .t1", "\
+            Aficionado a la informática desde joven, adquirí de manera autodidacta \
+            los conocimientos básicos de administración de sistemas. \
+            Después cursé estudios reglados de desarrollo de aplicaciones, y más tarde de web.");
+        setContentSelection("#about .t2", "\
+            En mi tiempo libre me gusta programar y aprender a manejar \
+            nuevas herramientas y lenguajes de programación.");
     }
     else {
         setContentName("title", "alamios&nbsp;&nbsp;|&nbsp;&nbsp;About");
